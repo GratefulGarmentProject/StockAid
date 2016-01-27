9519ccbeac9ebd8c10f64216391c7d9aba748807
Yasmin Fazelinia <yfazelinia@on-site.com>
2016-01-27 15:06:21 -0800

    Add Items index view and set instance variables in ItemsController#index.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
   def index
+    @categories = Category.all
+    if params[:category_id].present?
+      @items = Item.where(category_id: params[:category_id])
+      @category = Category.find(params[:category_id])
+    end
   end
 end
