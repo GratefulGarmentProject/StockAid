64a18fcec6a184a6f0ea7652ecae0488dcd2254d
Mike Virata-Stone <mjstone@on-site.com>
2016-01-27 00:51:43 -0800

    Basic role implementation Add fixtures and spec

--- app/models/concerns/users/info.rb
+++ app/models/concerns/users/info.rb
@@ -3,9 +3,22 @@ module Users
     extend ActiveSupport::Concern
 
     def super_admin?
+      role == "admin"
     end
 
     def admin?(organization)
+      super_admin? || role_for(organization) == "admin"
+    end
+
+    def organization_user_for(organization)
+      organization_users.find do |org_user|
+        org_user.organization_id == organization.id
+      end
+    end
+
+    def role_for(organization)
+      org_user = organization_user_for(organization)
+      org_user.role if org_user
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require "rails_helper"
 
 describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:root) { users(:root) }
+  let(:acme_root) { users(:acme_root) }
+  let(:acme_normal) { users(:acme_normal) }
+  let(:foo_inc_root) { users(:foo_inc_root) }
+
+  let(:acme) { organizations(:acme) }
+  let(:foo_inc) { organizations(:foo_inc) }
+
+  describe "#super_admin?" do
+    it "tells if the user is an admin for all of the site" do
+      expect(root).to be_a_super_admin
+
+      expect(acme_root).to_not be_a_super_admin
+      expect(acme_normal).to_not be_a_super_admin
+    end
+  end
+
+  describe "#admin?" do
+    it "tells if the user is an admin for a particular organization" do
+      expect(root.admin?(acme)).to be_truthy
+      expect(acme_root.admin?(acme)).to be_truthy
+      expect(foo_inc_root.admin?(foo_inc)).to be_truthy
+
+      expect(acme_normal.admin?(acme)).to be_falsey
+      expect(acme_root.admin?(foo_inc)).to be_falsey
+      expect(foo_inc_root.admin?(acme)).to be_falsey
+    end
+  end
 end
