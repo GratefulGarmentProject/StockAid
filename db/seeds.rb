b125be7a37bd02dad34385921a0116fd92e3e7cc
Yasmin Fazelinia <yfazelinia@on-site.com>
2016-01-27 15:07:30 -0800

    Seed sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  category_one = Category.create({ description: 'Tops' })
+  category_two = Category.create({ description: 'Bottoms' })
+
+  item_one = Item.create({ description: 'Sweatshirt', category_id: category_one.id})
+  item_two = Item.create({ description: 'Jeans', category_id: category_two.id})
+
+  Inventory.create({ current_quantity: 10, item_id: item_one.id })
+  Inventory.create({ current_quantity: 20, item_id: item_two.id })
+
