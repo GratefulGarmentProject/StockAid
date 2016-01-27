2af534d3edc583aab047b0d3e7c677086f339fc5
Sean Linke <slinke@on-site.com>
2016-01-26 23:13:16 -0800

    RUBOCOP FTW

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :orders
-  resources :items, path: '/inventory'
+  resources :items, path: "/inventory"
   resources :contacts
 
   # TODO: Update this to correct path
