c0d38214e0df68d16a8fe5ff319a80d3fbacaf67
Mike Virata-Stone, Jon Rieta, Bill McConaghy, Melanie Rosson, Kylr Avery, Thomas Eggenberger, Ivan Edwards, Kyle Ling, and Tim Moreno <opensource@on-site.com>
2016-01-26 16:28:23 -0800

    Add users and organizations

---
+++ db/migrate/20160126235148_create_organizations.rb
@@ -0,0 +1,12 @@
+class CreateOrganizations < ActiveRecord::Migration
+  def change
+    create_table :organizations do |t|
+      t.string :name, null: false
+      t.string :address, null: false
+      t.string :phone_number
+      t.string :email_address
+      t.timestamps null: false
+      t.index :name
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -54,4 +54,37 @@ ActiveRecord::Schema.define(version: 20160127010428) do
     t.datetime "updated_at"
   end
 
+  create_table "organizations", force: :cascade do |t|
+    t.string   "name",          null: false
+    t.string   "address",       null: false
+    t.string   "phone_number"
+    t.string   "email_address"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "organizations", ["name"], name: "index_organizations_on_name", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
+
 end
