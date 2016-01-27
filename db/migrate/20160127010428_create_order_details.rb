d6bbfabf2b7532c6238c2c03af746f54bb6bf8da
Rob Roberts <robr@pcisys.net>
2016-01-26 21:25:39 -0700

    Add index and foreign key constraint to order_details table

--- db/migrate/20160127010428_create_order_details.rb
+++ db/migrate/20160127010428_create_order_details.rb
@@ -1,8 +1,8 @@
 class CreateOrderDetails < ActiveRecord::Migration
   def change
     create_table :order_details do |t|
-      t.references :order, null: false
-      t.references :inventory, null: false
+      t.references :order, null: false, index: true
+      t.references :inventory, null: false, foreign_key: true
       t.column :quantity, :integer, null: false
       t.timestamps null: false
     end
