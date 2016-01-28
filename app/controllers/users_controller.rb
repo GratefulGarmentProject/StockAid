c0718fe698b0293ddd7aa819689eaff85663fbbf
Tek Hudson <tek.hudson@gmail.com>
2016-01-28 15:36:33 -0800

    Move edit user to view and add phone_number and address

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def update
     user = User.find params[:id]
 
-    user.update_attributes params.require("user").permit(:name, :email)
+    user.update_attributes params.require("user").permit(:name, :email, :phone_number, :address)
     redirect_to root_path if user.save
   end
 end
