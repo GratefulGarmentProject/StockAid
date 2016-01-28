353fbd5ccfec29fb61505910177eadd6fcb2e015
Mary and Yasmin <opensource@on-site.com>
2016-01-27 20:10:57 -0800

    Add Item#edit, Item#update, and the items edit form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,29 @@ class ItemsController < ApplicationController
       @category = Category.find(params[:category_id])
     end
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.description = items_params[:description]
+    @item.current_quantity = items_params[:current_quantity]
+    if @item.save
+      respond_to do |format|
+        format.html { redirect_to items_path(category_id: @item.category.id) }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to edit_item_path(@item.id) }
+      end
+    end
+  end
+
+  private
+
+  def items_params
+    params.require(:item).permit(:description, :current_quantity )
+  end
 end
