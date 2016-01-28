a7ee00b9f3edba6a42decda978c143af9028921a
Sean Linke <slinke@on-site.com>
2016-01-27 21:06:04 -0800

    Some styling fixes for navigation tabs and inventory page
    Removed a dupe resource references from routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  resources :contacts
+  resources :items, path: "/inventory"
   resources :orders
   resources :shipments
 
-  devise_for :users
-
-  resources :orders
-  resources :items, path: "/inventory"
-  resources :contacts
-
-  # TODO: Update this to correct path
   root to: "orders#index"
 end
