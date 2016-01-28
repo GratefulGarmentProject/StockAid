74e8e51e07459d8dbffeb76efa97d37ae681e521
Mike Virata-Stone <mjstone@on-site.com>
2016-01-28 09:23:16 -0800

    Fix rubocop error

--- spec/support/controllers_helper.rb
+++ spec/support/controllers_helper.rb
@@ -1,7 +1,16 @@
 module ControllersHelper
   def signed_in_user(user_fixture)
     user = users(user_fixture)
-    request.env["warden"] = double :authenticate! => user
+    stub_warden(user)
+    stub_controller(user)
+  end
+
+  def stub_warden(user)
+    request.env["warden"] = double
+    allow(request.env["warden"]).to receive(:authenticate!).and_return(user)
+  end
+
+  def stub_controller(user)
     allow(controller).to receive(:current_user).and_return(user)
   end
 end
