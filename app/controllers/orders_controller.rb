7433c5c6bffd0a7777cadf23ed04718b1ff51b7d
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2016-01-28 15:55:25 -0800

    Rubocop

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,11 +17,10 @@ class OrdersController < ApplicationController
 
     params[:order_details].each do |order_detail_id, quantity|
       found = @order.order_details.detect { |d| d.id.to_s == order_detail_id }
-      if found
-        next if found.quantity == quantity
-        found.quantity = quantity
-        found.save!
-      end
+      next unless found
+      next if found.quantity == quantity
+      found.quantity = quantity
+      found.save!
     end
 
     # [[order_details_id, quantity]]
@@ -29,6 +28,5 @@ class OrdersController < ApplicationController
   end
 
   def add_item
-
   end
 end
