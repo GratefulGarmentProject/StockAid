913d773f45292a524ddbe50fab4ef84e30c919e8
Mike Virata-Stone <mjstone@on-site.com>
2016-01-28 00:46:13 -0800

    Don't use url, we only need the paths Drop host, it is breaking my connection to the database Add column type so the change can be downed

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -5,11 +5,11 @@ class OrganizationsController < ApplicationController
 
   def update
     current_user.update_organization params
-    redirect_to organizations_url
+    redirect_to action: :index
   end
 
   def create
     current_user.create_organization params
-    redirect_to organizations_url
+    redirect_to action: :index
   end
 end
