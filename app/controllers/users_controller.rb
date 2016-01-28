8e5b2472991bcb3e66c11ed3de64d6edbed161b8
Sean Linke <slinke@on-site.com>
2016-01-28 15:03:43 -0800

    Rubocop -a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,8 @@
 class UsersController < ApplicationController
-
   def update
     user = User.find params[:id]
 
     user.update_attributes params.require("user").permit(:name, :email)
-    if user.save
-      redirect_to root_path
-    end
+    redirect_to root_path if user.save
   end
 end
