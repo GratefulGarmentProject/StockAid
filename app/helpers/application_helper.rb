22e3e1114d765872a9359bf91239ef10c608e05e
Sean Linke <slinke@on-site.com>
2016-01-27 23:38:38 -0800

    Add helper and partial for flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    case flash_type.to_sym
+    when :success
+      "alert-success"
+    when :error
+      "alert-danger"
+    when :alert
+      "alert-warning"
+    when :notice
+      "alert-info"
+    end
+  end
 end
