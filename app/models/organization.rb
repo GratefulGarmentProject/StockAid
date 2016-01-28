12078dc9c8b052a733a27fc206eb31a679577ebf
Bill McConaghy <wmcconag@gmail.com>
2016-01-28 17:37:30 -0500

    Making rubocop happy

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -7,17 +7,25 @@ class Organization < ActiveRecord::Base
   before_save :add_county_lat_lon
 
   def add_county_lat_lon
-    if self.changed_attributes.keys.include?("address")
-      begin
-        result = Geocoder.search(self.address).first
+    if changed_attributes.keys.include?("address")
+      result = fetch_geocoding_data
+      if result
         self.county = result.address_components.find { |component|
           component["types"].include?("administrative_area_level_2")
         }["short_name"]
         self.latitude = result.latitude
         self.longitude = result.longitude
-      rescue
-        Rails.logger.error("Error fetching geocoding info for #{self.address}")
       end
     end
   end
+
+  def fetch_geocoding_data
+    begin
+      result = Geocoder.search(address).first
+    rescue
+      Rails.logger.error("Error fetching geocoding info for #{address}")
+    end
+    result
+  end
+  private :fetch_geocoding_data
 end
--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,3 +1,3 @@
-if (ENV["STOCKAID_GOOGLE_API_KEY"])
-  Geocoder.configure(:api_key => ENV["STOCKAID_GOOGLE_API_KEY"])
+if ENV["STOCKAID_GOOGLE_API_KEY"]
+  Geocoder.configure(api_key: ENV["STOCKAID_GOOGLE_API_KEY"])
 end
