00c181355e2f42db67afac5ce04ea1471f8bdfa8
Yasmin Fazelinia <yfazelinia@on-site.com>
2016-01-27 22:16:15 -0800

    Add sizes column as an array to the categories table and serialize sizes in Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
   has_many :items
+  serialize :sizes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160128030034) do
+ActiveRecord::Schema.define(version: 20160128060914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
-    t.string   "description", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "description",              null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.text     "sizes",       default: [],              array: true
   end
 
   create_table "items", force: :cascade do |t|
