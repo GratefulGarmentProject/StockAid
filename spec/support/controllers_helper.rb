4c2275526f5571d70d2a5c3b32f80f20f0559aa8
Mike Virata-Stone <mjstone@on-site.com>
2016-01-28 08:19:56 -0800

    Fix specs and add user_manipulator.rb

--- spec/support/controllers_helper.rb
+++ spec/support/controllers_helper.rb
@@ -1,6 +1,7 @@
 module ControllersHelper
   def signed_in_user(user_fixture)
     user = users(user_fixture)
+    request.env["warden"] = double :authenticate! => user
     allow(controller).to receive(:current_user).and_return(user)
   end
 end
