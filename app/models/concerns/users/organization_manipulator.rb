56af5534dc3a91869f03f6375c4fec9a7cc68b4a
Mike Virata-Stone <mjstone@on-site.com>
2016-01-27 23:30:17 -0800

    Drop unused variable, update variable names for clarity, move partial to have format in name

--- app/models/concerns/users/organization_manipulator.rb
+++ app/models/concerns/users/organization_manipulator.rb
@@ -10,24 +10,24 @@ module Users
       admin?(organization)
     end
 
-    def can_update_organization_name?(_organization)
+    def can_update_organization_name?
       super_admin?
     end
 
     def create_organization(params)
       raise PermissionError unless can_create_organization?
-      organization = params.require(:organization)
-      Organization.create! organization.permit(:name, :address, :phone_number, :email)
+      org_params = params.require(:organization)
+      Organization.create! org_params.permit(:name, :address, :phone_number, :email)
     end
 
     def update_organization(params)
       org = Organization.find(params[:id])
       raise PermissionError unless can_update_organization?(org)
-      organization = params.require(:organization)
-      if can_update_organization_name?(org)
-        org.update! organization.permit(:name, :address, :phone_number, :email)
+      org_params = params.require(:organization)
+      if can_update_organization_name?
+        org.update! org_params.permit(:name, :address, :phone_number, :email)
       else
-        org.update! organization.permit(:address, :phone_number, :email)
+        org.update! org_params.permit(:address, :phone_number, :email)
       end
     end
   end
