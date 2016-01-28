0bd9af9b3a99d545bfd1aad52d3d7d1aed4bc27a
Thomas Eggenberger, Kyle Ling, Mike Virata-Stone <opensource@on-site.com>
2016-01-27 16:07:14 -0800

    Method to update an organization Reduce profiling specs to 2 methods (it was too spammy at 10)

--- app/models/concerns/users/organization_manipulator.rb
+++ app/models/concerns/users/organization_manipulator.rb
@@ -6,9 +6,28 @@ module Users
       super_admin?
     end
 
+    def can_update_organization?(organization)
+      admin?(organization)
+    end
+
+    def can_update_organization_name?(organization)
+      super_admin?
+    end
+
     def create_organization(params)
       raise PermissionError unless can_create_organization?
       Organization.create! params.slice(:name, :address, :phone_number, :email)
+    end
+
+    def update_organization(params)
+      org = Organization.find(params[:id])
+      raise PermissionError unless can_update_organization?(org)
+
+      if can_update_organization_name?(org)
+        org.update! params.slice(:name, :address, :phone_number, :email)
+      else
+        org.update! params.slice(:address, :phone_number, :email)
+      end
     end
   end
 end
