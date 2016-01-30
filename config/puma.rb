362eb8e74c435ae96af2dfa16090111cf54ccf66
Tek Hudson <tek.hudson@gmail.com>
2016-01-29 23:38:53 -0800

    rubocop -a

--- config/puma.rb
+++ config/puma.rb
@@ -2,15 +2,15 @@
 # https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
 # with some small changes
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+workers Integer(ENV["WEB_CONCURRENCY"] || 2)
 threads_count = Integer(1)
 threads threads_count, threads_count
 
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+port        ENV["PORT"]     || 3000
+environment ENV["RACK_ENV"] || "development"
 
 on_worker_boot do
   ActiveRecord::Base.establish_connection
