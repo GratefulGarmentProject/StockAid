dcc904c0e54697dd0c3a8cf3207fa6bdf66ce893
Sean Linke <slinke@on-site.com>
2016-01-31 21:59:07 -0800

    Make paper trail use a user's name instead of id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_filter :set_paper_trail_whodunnit
+
+  def user_for_paper_trail
+    user_signed_in? ? current_user.name : 'Unknown'
+  end
 
   private_class_method def self.active_tab(tab, *options)
     before_action(*options) { @active_tab = tab }
