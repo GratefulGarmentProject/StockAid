2b224cde72d919c696913e1a25b3332d0eb3e177
Sean Linke <slinke@on-site.com>
2016-02-05 00:21:05 -0800

    Changes for category creation/edit
    TODO:
    * Maybe break the common portion of the form out

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,8 @@ class Category < ActiveRecord::Base
   has_many :items
   validates :description, presence: true
 
+  default_scope { order("upper(description)") }
+
   def self.sizes_array(sizes_params)
     sizes_params.present? ? sizes_params.split(",").map(&:strip) : []
   end
