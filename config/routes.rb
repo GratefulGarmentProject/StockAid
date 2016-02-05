35765f219c0c4b5366d9f80d70bf70920deac328
Sean Linke <slinke@on-site.com>
2016-02-05 00:19:34 -0800

    Enables base categories CRUD routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :categories, only: [:create, :edit, :update, :destroy]
+  resources :categories
   resources :items, path: "/inventory"
   resources :orders do
     post :show_order_dialog
