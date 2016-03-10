39f604b3c14e010eb41ca657ac1eda556c2ff2d4
Sean Linke <slinke@on-site.com>
2016-03-10 15:33:29 -0800

    Add validation to ensure valid order detail creation
    Do not allow order details without a valid quantity

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,14 +2,18 @@ class OrderDetail < ActiveRecord::Base
   belongs_to :order
   belongs_to :item
 
+  validate :should_update_item_requested_quantity
+
   scope :for_order, ->(order_id) { where(order_id: order_id) }
 
-  before_create do
-    # Ensure the ordered quantity can be allocated by the item
-    if quantity <= item.current_quantity
-      item.current_quantity -= quantity
-      item.requested_quantity += quantity
+  def should_update_item_requested_quantity
+    if self.quantity >= 0
+      # Update subject item requested quantity.
+      item.requested_quantity += self.quantity
       item.save
+      return true
     end
+
+    false
   end
 end
