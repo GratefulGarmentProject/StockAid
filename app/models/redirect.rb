6e7e462ee50446dd0c9681ed39b35c3909f59581
Tek Hudson <tek.hudson@gmail.com>
2016-04-04 09:52:07 -0700

    Redirect to method argument now has default of empty array, minor url_helper corrections

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -3,7 +3,7 @@ class Redirect
   class NotAllowedError < StandardError; end
   class NotImplimentedError < StandardError; end
 
-  def self.to(default, params, allow:)
+  def self.to(default, params, allow: [])
     return default if params[:redirect_to].blank?
     allow = [allow].flatten.map(&:to_s)
     raise NotAllowedError if allow.exclude?(params[:redirect_to])
@@ -16,7 +16,7 @@ class Redirect
     when "orders"
       Rails.application.routes.url_helpers.orders_path
     when "order"
-      Rails.application.routes.url_helpers.order_path(id_from(params))
+      Rails.application.routes.url_helpers.edit_order_path(id_from(params))
     when "inventory"
       Rails.application.routes.url_helpers.items_path()
     when "category"
@@ -24,11 +24,11 @@ class Redirect
     when "organizations"
       Rails.application.routes.url_helpers.organizations_path
     when "organization"
-      Rails.application.routes.url_helpers.organization_path(id_from(params))
+      Rails.application.routes.url_helpers.edit_organization_path(id_from(params))
     when "users"
       Rails.application.routes.url_helpers.users_path
     when "user"
-      Rails.application.routes.url_helpers.user_path(id_from(params))
+      Rails.application.routes.url_helpers.edit_user_path(id_from(params))
     else
       raise NotImplimentedError
     end
