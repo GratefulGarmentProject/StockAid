afa65496c9dfa0591f5dff967eb76c033767c753
Tek Hudson <tek.hudson@gmail.com>
2016-04-04 09:50:29 -0700

    Add href path to user_helper json call

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,11 @@
 module UsersHelper
   def additional_organizations_json(user, organizations)
     organizations.drop(1).map do |organization|
-      { name: organization.name, role: user.role_at(organization) }
+      {
+        name: organization.name,
+        role: user.role_at(organization),
+        href: edit_organization_path(organization, redirect_to: "users")
+      }
     end.to_json
   end
 end
