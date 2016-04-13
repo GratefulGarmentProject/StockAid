ffbb6cb7929e49fa6f79cb2add206d0df769005e
Tek Hudson <tek.hudson@gmail.com>
2016-04-12 22:59:09 -0700

    Change price from numeric to decimal

--- db/migrate/20160413051610_add_sku_and_price_to_item.rb
+++ db/migrate/20160413051610_add_sku_and_price_to_item.rb
@@ -1,6 +1,6 @@
 class AddSkuAndPriceToItem < ActiveRecord::Migration
   def change
     add_column :items, :sku, :string
-    add_column :items, :price, :numeric
+    add_column :items, :price, :decimal, :precision => 8, :scale => 2
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,14 +23,14 @@ ActiveRecord::Schema.define(version: 20160413051610) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.string   "description",                    null: false
-    t.integer  "category_id",                    null: false
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.integer  "current_quantity",   default: 0, null: false
-    t.integer  "requested_quantity", default: 0, null: false
+    t.string   "description",                                            null: false
+    t.integer  "category_id",                                            null: false
+    t.datetime "created_at",                                             null: false
+    t.datetime "updated_at",                                             null: false
+    t.integer  "current_quantity",                           default: 0, null: false
+    t.integer  "requested_quantity",                         default: 0, null: false
     t.string   "sku"
-    t.decimal  "price"
+    t.decimal  "price",              precision: 8, scale: 2
   end
 
   create_table "order_details", force: :cascade do |t|
