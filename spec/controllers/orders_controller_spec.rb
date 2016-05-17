d58d5e2b0cd0e7c5e09e62b6aab10dbb949ae537
Tek Hudson <tek.hudson@gmail.com>
2016-05-17 16:42:59 -0700

    Start writing order specs

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -1,4 +1,15 @@
 require "rails_helper"
 
 describe OrdersController, type: :controller do
+  describe "POST create" do
+    it "creates an order assigned to the selected organization"
+    it "fails if user isn't affiliated with selected organization"
+    it "fails if organization doesn't exist"
+    it "creates an order that contains items requested"
+    it "fails if the quantity of an item requested is greater than available"
+    it "fails if the quantity of an item requested is 0"
+    it "fails if the quantity of an item requested is negative"
+    it "fails if the same item is requested multiple times"
+    it "fails if there is a partial item description"
+  end
 end
