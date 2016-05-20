0091850e2d55daf0f1d8642355811839d8f1f4cc
Tek Hudson <tek.hudson@gmail.com>
2016-05-19 21:15:51 -0700

    Create order_detail param validation method

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,3 +1,6 @@
+class InvalidOrderDetailsError < StandardError
+end
+
 class OrderDetail < ActiveRecord::Base
   belongs_to :order
   belongs_to :item
@@ -18,5 +21,16 @@ class OrderDetail < ActiveRecord::Base
       item_id: item_id,
       quantity: quantity
     }
+  end
+
+  def self.order_details_valid?(params)
+    items = Item.find params[:order][:order_details][:item_id]
+    params[:order][:order_details][:item_id].each_with_index { |item_id, index|
+      item = items.select { |item| item.id == item_id.to_i }.first
+      quantity_available = item.current_quantity - item.requested_quantity
+      raise InvalidOrderDetailsError if quantity_available <= 0
+      raise InvalidOrderDetailsError if quantity_available < params[:order][:order_details][:quantity][index].to_i
+    }
+    true
   end
 end
