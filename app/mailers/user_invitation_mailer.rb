36dc8c012dc3037098fcc4e7b1eb29905cb2f81f
Mike Virata-Stone <mjstone@on-site.com>
2016-06-10 12:07:01 -0700

    Add the final text view

--- app/mailers/user_invitation_mailer.rb
+++ app/mailers/user_invitation_mailer.rb
@@ -1,4 +1,4 @@
-class UserInvitationMailer < ActionMailer::Base
+class UserInvitationMailer < ApplicationMailer
   def invite(invitation)
     @invitation = invitation
     mail to: @invitation.email, subject: "You've been invited to #{ActionMailer::Base.default_url_options[:host]}!"
