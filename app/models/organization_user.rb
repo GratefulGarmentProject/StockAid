9f9211d52b7dd2bf0ed2f51da75703d453bf7dfd
Tek Hudson <tek.hudson@gmail.com>
2016-07-07 16:17:08 -0700

    Rubocop fix

--- app/models/organization_user.rb
+++ app/models/organization_user.rb
@@ -2,7 +2,7 @@ class OrganizationUser < ActiveRecord::Base
   belongs_to :organization
   belongs_to :user
   validates :organization_id, uniqueness: { scope: :user_id,
-  message: "already has a user with this email" }
+                                            message: "already has a user with this email" }
 
   def invite_mailer(invited_by)
     OrganizationUserMailer.invite(self, invited_by)
