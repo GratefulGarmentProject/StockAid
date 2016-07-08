680de1aeb373501d7d58d64f64f0f7d855586d7f
Tek Hudson <tek.hudson@gmail.com>
2016-07-08 11:49:28 -0700

    Add uniqueness to order_id and item_id in OrderDetails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629195014) do
+ActiveRecord::Schema.define(version: 20160708184341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20160629195014) do
     t.decimal  "value",      precision: 8, scale: 2
   end
 
+  add_index "order_details", ["order_id", "item_id"], name: "index_order_details_on_order_id_and_item_id", unique: true, using: :btree
   add_index "order_details", ["order_id"], name: "index_order_details_on_order_id", using: :btree
 
   create_table "orders", force: :cascade do |t|
