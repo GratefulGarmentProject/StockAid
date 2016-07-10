c5fb7db27f648e7a499259313fbad7520557c779
Kareem Bishi <icodeclean@gmail.com>
2016-07-10 19:05:07 -0400

    Enhanced history details with better descriptions and format

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -3,4 +3,17 @@ module ItemsHelper
     return "System" unless value
     User.find(value).name
   end
+
+  def version_details(version)
+    parameters = {
+      details: version.edit_source,
+      amount: version.edit_amount,
+      previous_total: version.changeset["current_quantity"].first,
+      new_total: version.changeset["current_quantity"].last
+    }
+
+    parameters[:amount_description] = t(version.edit_method, parameters.merge(scope: [:history, :item, :method]))
+    parameters[:reason] = t(version.edit_reason, parameters.merge(scope: [:history, :item, :reason]))
+    t(version.event, parameters.merge(scope: [:history, :item, :event]))
+  end
 end
