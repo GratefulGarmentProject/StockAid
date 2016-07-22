9ea9c1261ef4f148892a802ae840413c67a41a22
Tek Hudson <tek.hudson@gmail.com>
2016-07-22 15:05:34 -0700

    Add backups to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   end
 
   resource :backup, only: :show
+  resource :export, only: :show
 
   get "/.well-known/acme-challenge/:id" => "letsencrypt#authenticate"
 
