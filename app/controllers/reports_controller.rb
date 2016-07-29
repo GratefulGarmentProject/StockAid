534c9d6705a5f31650f9261bad79f69e0f64f919
Tek Hudson <tek.hudson@gmail.com>
2016-07-29 12:30:22 -0700

    Fix county name on reports page

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,8 +5,8 @@ class ReportsController < ApplicationController
 
   def index
     @org_by_county = {}
-    Organization.counties.sort_by(&:downcase).each do |county_name|
-      @org_by_county[county_name] = Organization.where(county: county_name)
+    Organization.counties.sort_by { |county| (county.presence || "no county").downcase }.each do |county_name|
+      @org_by_county[county_name.presence || "No County"] = Organization.where(county: county_name)
     end
   end
 end
