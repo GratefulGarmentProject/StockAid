68b35395e435db71c6db230f0a5791b50b419ebb
Frank Aiello <frankvaiello@gmail.com>
2016-10-27 21:48:25 -0700

    Add inventory view for clients

--- app/models/concerns/users/item_manipulator.rb
+++ app/models/concerns/users/item_manipulator.rb
@@ -2,8 +2,12 @@ module Users
   module ItemManipulator
     extend ActiveSupport::Concern
 
-    def can_view_or_edit_items?
+    def can_view_and_edit_items?
       super_admin?
+    end
+
+    def can_view_items?
+      true
     end
   end
 end
