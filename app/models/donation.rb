28d4ca56c744f06a5f0ce5d67fb697306e96f7c8
Mike Virata-Stone <mjstone@on-site.com>
2017-01-13 02:34:09 -0800

    Fill in the model to create the donation

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,18 @@
 class Donation
   def self.create_donation!(creator, params)
+    donation_params = params.require(:donation)
+
+    donation_params[:donation_details].fetch(:item_id, []).each_with_index do |item_id, i|
+      quantity = donation_params[:donation_details][:quantity][i]
+      item = Item.find(item_id)
+      item.mark_event(
+        edit_amount: quantity,
+        edit_method: "add",
+        edit_reason: "donation",
+        edit_source: donation_params[:description]
+      )
+
+      item.save!
+    end
   end
 end
