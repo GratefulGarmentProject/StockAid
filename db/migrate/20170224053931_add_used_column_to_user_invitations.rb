29fde1945f1c7718ae68612dd9c56d5c4aae5292
Tek Hudson <tek.hudson@gmail.com>
2017-02-24 13:38:42 -0800

    Update existing user invitations

--- db/migrate/20170224053931_add_used_column_to_user_invitations.rb
+++ db/migrate/20170224053931_add_used_column_to_user_invitations.rb
@@ -1,5 +1,15 @@
 class AddUsedColumnToUserInvitations < ActiveRecord::Migration
   def change
-    add_column :user_invitations, :used, :boolean, default: false
+    transaction do
+      add_column :user_invitations, :used, :boolean, default: false
+
+      uis = UserInvitations.all
+      uis.each do |ui|
+        user = User.where(email: ui.email)
+        user_already_at_org = false
+        user_already_at_org = OrganizationUser.where(id: user.id, organization: ui.organization_id) if user.present?
+        ui.update!(used: true) if user_already_at_org.present?
+      end
+    end
   end
 end
