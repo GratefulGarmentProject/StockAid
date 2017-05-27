8da8fb30ce3aa90ca3ebc60c7432ddbc8b879543
Tek Hudson <tek.hudson@gmail.com>
2017-05-26 22:27:47 -0700

    Remove commented out code

--- app/models/order_notes_updater.rb
+++ app/models/order_notes_updater.rb
@@ -9,8 +9,6 @@ class OrderNotesUpdater
   def update
     return unless new_order_notes_present?
     @notes = params[:order][:order_notes][:text]
-    # update_exsisting
-    # zero_out
     add_new_details
   end
 
@@ -22,25 +20,7 @@ class OrderNotesUpdater
     end
   end
 
-  # def note_ids_to_update
-  #   @note_ids_to_update ||= original_note_ids & new_note_ids
-  # end
-
-  # def original_note_ids
-  #   @original_note_ids ||= order_notes.map(&:id)
-  # end
-
-  # def order_notes
-  #   order.order_notes
-  # end
-
   def new_order_notes_present?
     params[:order].present? && params[:order][:order_notes].present?
   end
-
-  # def zero_out
-  #   note_ids_to_zero.each do |note_id|
-  #     order_notes_hash[note_id].quantity = 0
-  #   end
-  # end
 end
