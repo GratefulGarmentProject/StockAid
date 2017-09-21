7509e22a783efd349e7153ed5ff7c3265ceae6a9
Tek Hudson <tek.hudson@gmail.com>
2017-09-21 08:41:07 -0700

    Update organizations with all methods for deletion

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,8 @@
 class Organization < ActiveRecord::Base
+  def self.default_scope
+    not_deleted
+  end
+
   has_many :organization_users
   has_many :users, through: :organization_users
   has_many :orders
@@ -9,15 +13,56 @@ class Organization < ActiveRecord::Base
 
   before_save :add_county
 
-  def primary_address
-    addresses.first
+  def self.find_any(id)
+    unscoped.find(id)
+  end
+
+  def self.find_deleted(id)
+    deleted.find id
+  end
+
+  def self.deleted
+    unscoped.where.not(deleted_at: nil)
+  end
+
+  def self.not_deleted
+    where(deleted_at: nil)
+  end
+
+  def soft_delete
+    return false if open_orders.present?
+
+    transaction do
+      organization_users.map(&:destroy!) if organization_users.present?
+
+      self.deleted_at = Time.zone.now
+      save
+    end
+  end
+
+  def restore
+    self.deleted_at = nil
+    save!
   end
 
   def self.counties
     pluck(:county).uniq
   end
 
+  def deleted?
+    deleted_at != nil
+  end
+
+  def primary_address
+    addresses.first
+  end
+
+  def open_orders
+    @_open_orders ||= orders.select { |order| order.open? }
+  end
+
   private
+
 
   def add_county
     return if county.present? || primary_address.blank?
