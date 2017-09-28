c4e3740e08ed50ed746dd8f1c9ed7c3cf6352b0c
Tek Hudson <tek.hudson@gmail.com>
2017-09-27 17:11:58 -0700

    Include all files in lib exceptions folder

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path("../application", __FILE__)
 
+# Load custom exceptions
+Dir[Rails.root.join('lib/exceptions/**/*.rb')].each { |f| require f }
+
 # Initialize the Rails application.
 Rails.application.initialize!
