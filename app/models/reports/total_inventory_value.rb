93990bc252fd2af5e0e450c6e3db420dc85b311d
edwin <edwin@homerlogistics.com>
2017-10-15 20:07:09 -0400

    Update TotalInventoryValue to accept date

--- app/models/reports/total_inventory_value.rb
+++ app/models/reports/total_inventory_value.rb
@@ -4,7 +4,7 @@ module Reports
       if params[:category_id].present?
         Reports::TotalInventoryValue::SingleCategory.new(params)
       else
-        Reports::TotalInventoryValue::AllCategories.new
+        Reports::TotalInventoryValue::AllCategories.new(params)
       end
     end
 
@@ -13,35 +13,51 @@ module Reports
 
       def initialize(params)
         @category = Category.find(params[:category_id])
+        @params = params
       end
 
       def each
         category.items.order(:description).each do |item|
-          yield item.description, item.total_value
+          yield item.description, item.total_value(at: date)
         end
       end
 
       def total_value
         category.value
       end
+
+      def date
+        if @params[:date].present?      
+          Date.strptime(@params[:date], "%m/%d/%Y").end_of_day
+        end
+      end
+
     end
 
     class AllCategories
       attr_reader :categories
 
-      def initialize
+      def initialize(params)
         @categories = Category.all
+        @params = params
       end
 
       def each
         categories.each do |category|
-          yield category.description, category.value
+          yield category.description, category.value(at: date)
         end
       end
 
       def total_value
         categories.to_a.sum(&:value)
       end
+
+      def date
+        if @params[:date].present?      
+          Date.strptime(@params[:date], "%m/%d/%Y").end_of_day
+        end
+      end
+
     end
   end
 end
