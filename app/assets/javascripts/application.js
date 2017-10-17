4fd148288af1a84cc2d1cb1cefa24eddcf8620d7
Tek Hudson <tek.hudson@gmail.com>
2017-10-17 16:52:56 -0700

    Add twitter typeahead rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require twitter/typeahead
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require turbolinks
