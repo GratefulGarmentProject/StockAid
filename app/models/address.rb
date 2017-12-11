4047a669c137b7e6098c578011f993e3a3036e40
Tek Hudson <tek.hudson@gmail.com>
2017-12-11 12:43:54 -0800

    When a Organization changes address send an email to notify admins

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,15 @@
 class Address < ActiveRecord::Base
   belongs_to :organization
+
+  after_update :email_address_changes, if: :changed?
+
   def to_s
     address
+  end
+
+  private
+
+  def email_address_changes
+    AddressChangeMailer.change(organization, self.changes[:address][0], self.changes[:address][1]).deliver_now
   end
 end
