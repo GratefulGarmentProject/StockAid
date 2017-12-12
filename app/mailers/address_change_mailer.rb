a63f0c237233366a90e344948a3ce70a0e1b5acd
Tek Hudson <tek.hudson@gmail.com>
2017-12-11 21:08:21 -0800

    Fix multiple admin email logic

--- app/mailers/address_change_mailer.rb
+++ app/mailers/address_change_mailer.rb
@@ -1,36 +1,11 @@
 class AddressChangeMailer < ActionMailer::Base
-  layout 'admin_mailer_large'
+  layout "admin_mailer_large"
 
-  def change(organization, before, after)
+  def change(user, organization, before, after)
     @organization = organization
     @before = before
     @after = after
 
-    all_admins_sql = user.include(:organization_users).join(organization_users, Arel::Nodes::OuterJoin).on(users[:id].eq(organization_users[:user_id])).where(sys_admins.or(org_admins)).to_sql
-
-    admins = User.includes(:organization_users).find_by_sql(all_admins_sql)
-
-    admins.each do |admin|
-      mail to: admin.email, subject: "Address change for #{@organization.name}"
-    end
-  end
-
-  private
-
-  def user
-    User.arel_table
-  end
-
-  def organization_users
-    OrganizationUser.arel_table
-  end
-
-  def sys_admins
-    user[:role].eq('admin')
-  end
-
-  def org_admins
-    organization_users[:organization].eq(@organization)
-      .and(organization_users[:role].eq('admin'))
+    mail to: user.email, subject: "Address change for #{@organization.name}"
   end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -10,6 +10,15 @@ class Address < ActiveRecord::Base
   private
 
   def email_address_changes
-    AddressChangeMailer.change(organization, self.changes[:address][0], self.changes[:address][1]).deliver_now
+    system_admins = User.where(role: "admin")
+    org_admins = User.joins(:organization_users).where(
+      organization_users: { organization: organization, role: "admin" }
+    )
+
+    admins_to_email = system_admins + org_admins
+
+    admins_to_email.each do |admin|
+      AddressChangeMailer.change(admin, organization, changes[:address][0], changes[:address][1]).deliver_now
+    end
   end
 end
