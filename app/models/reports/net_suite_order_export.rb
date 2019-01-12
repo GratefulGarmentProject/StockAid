c19c853d18bac79badb18aace692a67d792eec98
Mike Virata-Stone <stonemj@gmail.com>
2019-01-12 12:59:43 -0800

    Fix the issues in #629

--- app/models/reports/net_suite_order_export.rb
+++ app/models/reports/net_suite_order_export.rb
@@ -2,17 +2,9 @@ require "csv"
 
 module Reports
   class NetSuiteOrderExport
-    FIELDS = %w(tranId customFormRef customerRef tranDate postingPeriodRef currencyRef exchangeRate termsRef
-                leadSourceRef PO# excludeCommissions memo salesEffectiveDate toBePrinted toBeEmailed email toBeFaxed
-                fax billAttention billAddressee billAddr1 billAddr2 billCity billState billZip billCountry billPhone
-                shipAttention shipAddressee shipAddr1 shipAddr2 shipCity shipState shipZip shipCountry shipPhone
-                shipMethodRef shipDate contractStartDate renewalDate shipCost FOB customerMessageRef salesRepRef
-                departmentRef classRef locationRef isTaxable discount-discountItemRef discount-discountRate
-                discount-discountTotal tax-taxItemRef tax-taxRate tax-taxTotal revRecScheduleRef revRecStartDate
-                revRecEndDate itemLine_itemRef itemLine_quantity itemLine_serialNumbers itemLine_salesPrice
-                itemLine_amount itemLine_description itemLine_isTaxable itemLine_priceLevelRef itemLine_departmentRef
-                itemLine_classRef itemLine_locationRef itemLine_revRecScheduleRef itemLine_revRecStartDate
-                itemLine_revRecEndDate).freeze
+    FIELDS = %w(tranId customerRef tranDate memo shipAttention shipAddressee shipAddr1 shipAddr2 shipCity shipState
+                shipZip itemLine_itemRef itemLine_quantity itemLine_serialNumbers itemLine_salesPrice itemLine_amount
+                itemLine_description).freeze
 
     def to_csv
       CSV.generate(headers: true) do |csv|
@@ -25,19 +17,25 @@ module Reports
     end
 
     def each
-      Order.includes(:user, order_details: :item).each do |order|
-        order.order_details.each do |detail|
-          yield Row.new(order, detail)
+      Order.includes(:user, :organization_unscoped, order_details: :item).order(:id).each_with_index do |order, i|
+        # Note: If the sort of the included class (OrderDetail) were done in the
+        # order() above, it would do a single query instead of 1 query for each
+        # class loaded, so use sort_by on the small set of details rather than
+        # doing a super large single query.
+        order.order_details.sort_by(&:id).each do |detail|
+          yield Row.new(order, detail, i)
         end
       end
     end
 
     class Row
-      attr_reader :order, :order_detail
+      attr_reader :order, :order_detail, :index
 
-      def initialize(order, order_detail)
+      def initialize(order, order_detail, index)
         @order = order
         @order_detail = order_detail
+        @index = index
+        extract_address
       end
 
       # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
@@ -45,67 +43,28 @@ module Reports
         case field
         when "tranId"
           "Order-#{order.id}"
-        when "customFormRef"
         when "customerRef"
+          order.organization_unscoped.name
         when "tranDate"
           order.order_date.strftime("%m/%d/%Y")
-        when "postingPeriodRef"
-        when "currencyRef"
-        when "exchangeRate"
-        when "termsRef"
-        when "leadSourceRef"
-        when "PO#"
-        when "excludeCommissions"
         when "memo"
-        when "salesEffectiveDate"
-        when "toBePrinted"
-        when "toBeEmailed"
-        when "email"
-        when "toBeFaxed"
-        when "fax"
-        when "billAttention"
-        when "billAddressee"
-        when "billAddr1"
-        when "billAddr2"
-        when "billCity"
-        when "billState"
-        when "billZip"
-        when "billCountry"
-        when "billPhone"
+          memo
         when "shipAttention"
-          "Order placed by #{order.user.name}"
+          @ship_attention
         when "shipAddressee"
           order.ship_to_name
         when "shipAddr1"
-          order.ship_to_address
+          @ship_address1
         when "shipAddr2"
+          @ship_address2
         when "shipCity"
+          @ship_city
         when "shipState"
+          @ship_state
         when "shipZip"
-        when "shipCountry"
-        when "shipPhone"
-          order.user.primary_number
-        when "shipMethodRef"
-        when "shipDate"
-        when "contractStartDate"
-        when "renewalDate"
-        when "shipCost"
-        when "FOB"
-        when "customerMessageRef"
-        when "salesRepRef"
-        when "departmentRef"
-        when "classRef"
-        when "locationRef"
-        when "isTaxable"
-        when "discount-discountItemRef"
-        when "discount-discountRate"
-        when "discount-discountTotal"
-        when "tax-taxItemRef"
-        when "tax-taxRate"
-        when "tax-taxTotal"
-        when "revRecScheduleRef"
-        when "revRecStartDate"
-        when "revRecEndDate"
+          @ship_zip
+        when "lineId"
+          index + 1
         when "itemLine_itemRef"
           "item-#{order_detail.item.id}"
         when "itemLine_quantity"
@@ -115,17 +74,26 @@ module Reports
         when "itemLine_salesPrice"
           ActionController::Base.helpers.number_to_currency(order_detail.value, unit: "$", precision: 2)
         when "itemLine_amount"
+          order_detail.total_value
         when "itemLine_description"
           order_detail.item.description
-        when "itemLine_isTaxable"
-        when "itemLine_priceLevelRef"
-        when "itemLine_departmentRef"
-        when "itemLine_classRef"
-        when "itemLine_locationRef"
-        when "itemLine_revRecScheduleRef"
-        when "itemLine_revRecStartDate"
-        when "itemLine_revRecEndDate"
         end
+      end
+
+      def memo
+        ["Order placed by #{order.user.name}", order.notes.presence].compact.join("\n")
+      end
+
+      private
+
+      def extract_address
+        result = AddressParser.new.parse(order.ship_to_address)
+        @ship_attention = result[:attention]
+        @ship_address1 = result[:address1]
+        @ship_address2 = result[:address2]
+        @ship_city = result[:city]
+        @ship_state = result[:state]
+        @ship_zip = result[:zip]
       end
     end
   end
