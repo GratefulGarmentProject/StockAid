d2651e588460948194e112014ff5480de10c1ff3
Tek Hudson <tek.hudson@gmail.com>
2019-03-05 21:05:11 -0800

    Add order refactor in a similar fashion to donations, one line per order, with limited info.

--- app/models/reports/net_suite/order_export.rb
+++ app/models/reports/net_suite/order_export.rb
@@ -3,43 +3,29 @@ module Reports
     class OrderExport
       include CsvExport
 
-      FIELDS = %w(tranId status customerRef tranDate memo shipAttention shipAddressee shipAddr1 shipAddr2 shipCity
-                  shipState shipZip lineId itemLine_itemRef itemLine_quantity itemLine_serialNumbers itemLine_salesPrice
-                  itemLine_amount itemLine_description).freeze
+      FIELDS = %w(orderDate organizationName organizationExternalId
+                  organizationExternalType memo value revenueStream).freeze
 
       def each
         Order.includes(:user, :organization_unscoped, order_details: :item).order(:id).each do |order|
-          # Note: If the sort of the included class (OrderDetail) were done in the
-          # order() above, it would do a single query instead of 1 query for each
-          # class loaded, so use sort_by on the small set of details rather than
-          # doing a super large single query.
-          order.order_details.sort_by(&:id).each_with_index do |detail, i|
-            yield Row.new(order, detail, i)
-          end
+          yield Row.new(order)
         end
       end
 
       class Row
-        attr_reader :order, :order_detail, :index,
-                    :ship_attention, :ship_addr1, :ship_addr2, :ship_city, :ship_state, :ship_zip
+        attr_reader :order, :organization
+
         delegate :status, to: :order
+        delegate :name, to: :organization, prefix: true
+        delegate :external_id, to: :organization, prefix: true
+        delegate :external_type, to: :organization, prefix: true
 
-        def initialize(order, order_detail, index)
+        def initialize(order)
           @order = order
-          @order_detail = order_detail
-          @index = index
-          extract_address
+          @organization = order.organization_unscoped
         end
 
-        def tran_id
-          "Order-#{order.id}"
-        end
-
-        def customer_ref
-          order.organization_unscoped.name
-        end
-
-        def tran_date
+        def order_date
           order.order_date.strftime("%m/%d/%Y")
         end
 
@@ -47,48 +33,13 @@ module Reports
           ["Order placed by #{order.user.name}", order.notes.presence].compact.join("\n")
         end
 
-        def ship_addressee
-          order.ship_to_name
+        def value
+          order.value.to_s
         end
 
-        def line_id
-          index + 1
-        end
-
-        def item_line_item_ref
-          "item-#{order_detail.item.id}"
-        end
-
-        def item_line_quantity
-          order_detail.quantity
-        end
-
-        def item_line_serial_numbers
-          order_detail.item.sku
-        end
-
-        def item_line_sales_price
-          ActionController::Base.helpers.number_to_currency(order_detail.value, unit: "$", precision: 2)
-        end
-
-        def item_line_amount
-          ActionController::Base.helpers.number_to_currency(order_detail.total_value, unit: "$", precision: 2)
-        end
-
-        def item_line_description
-          order_detail.item.description
-        end
-
-        private
-
-        def extract_address
-          result = AddressParser.new.parse(order.ship_to_address)
-          @ship_attention = result[:attention]
-          @ship_addr1 = result[:address1]
-          @ship_addr2 = result[:address2]
-          @ship_city = result[:city]
-          @ship_state = result[:state]
-          @ship_zip = result[:zip]
+        def revenue_stream
+          # Impliment with revenue streams
+          nil
         end
       end
     end
