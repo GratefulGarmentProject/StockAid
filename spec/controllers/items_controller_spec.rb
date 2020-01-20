90bed5bc5124838e46d44d9646b3b909db43f61c
Tek Hudson <tek.hudson@gmail.com>
2020-01-20 10:05:06 -0800

    Add some controller specs for items

---
+++ spec/controllers/items_controller_spec.rb
@@ -0,0 +1,57 @@
+require "rails_helper"
+
+describe ItemsController, type: :controller do
+  let(:category1)  { categories(:flip_flops) }
+  let(:category2)  { categories(:pants) }
+  let(:item1)      { items(:small_flip_flops) }
+  let(:item2)      { items(:large_flip_flops) }
+  let(:item3)      { items(:small_pants) }
+  let(:item4)      { items(:small_pants) }
+
+  describe "GET#index" do
+    it "returns the correct variables" do
+      signed_in_user :root
+
+      get :index, params: { category_id: category1.id }
+
+      expect(assigns(:category)).to eq(category1)
+      expect(assigns(:categories)).to include(category1, category2)
+      expect(assigns(:items)).to include(item1, item2, item3, item4)
+    end
+
+    context "when an item is deleted" do
+      it "does not include soft_deleted items" do
+        signed_in_user :root
+        item1.soft_delete
+
+        get :index
+
+        expect(assigns(:categories)).to include(category1, category2)
+        expect(assigns(:items)).to include(item2, item3, item4)
+      end
+    end
+
+    context "when a cateogry is deleted" do
+      it "does not include soft_deleted categories" do
+        signed_in_user :root
+        category1.soft_delete
+
+        get :index
+
+        expect(assigns(:categories)).not_to eq(category1)
+      end
+    end
+  end
+
+  describe "GET#deleted items" do
+    it "returns the correct items" do
+      signed_in_user :root
+
+      item2.soft_delete
+
+      get :deleted
+
+      expect(assigns(:items)).to eq([item2])
+    end
+  end
+end
