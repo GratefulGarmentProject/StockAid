f6c48b12a0f13ee5fa95d983e333d90772b6edc9
Tek Hudson <tek.hudson@gmail.com>
2020-01-20 21:32:55 -0800

    Fix broken specs

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,12 +1,13 @@
 require "rails_helper"
 
-Rspec.describe ItemsController, type: :controller do
-  let(:category1)  { categories(:flip_flops) }
-  let(:category2)  { categories(:pants) }
-  let(:item1)      { items(:small_flip_flops) }
-  let(:item2)      { items(:large_flip_flops) }
-  let(:item3)      { items(:small_pants) }
-  let(:item4)      { items(:small_pants) }
+describe ItemsController, type: :controller do
+  let(:category1)    { categories(:flip_flops) }
+  let(:category2)    { categories(:pants) }
+  let(:item1)        { items(:small_flip_flops) }
+  let(:item2)        { items(:large_flip_flops) }
+  let(:deleted_item) { items(:deleted_flip_flops) }
+  let(:item3)        { items(:small_pants) }
+  let(:item4)        { items(:small_pants) }
 
   describe "GET#index" do
     it "returns the correct variables" do
@@ -14,31 +15,21 @@ Rspec.describe ItemsController, type: :controller do
 
       get :index, params: { category_id: category1.id }
 
-      expect(assigns(:category)).to eq(category1)
+      expect(assigns(:category)).to   eq(category1)
+      expect(assigns(:items)).to      include(item1, item2)
       expect(assigns(:categories)).to include(category1, category2)
-      expect(assigns(:items)).to include(item1, item2, item3, item4)
     end
 
-    context "when an item is deleted" do
-      it "does not include soft_deleted items" do
+    context "when there are deleted items in the category" do
+      it "does not include deleted items" do
         signed_in_user :root
         item1.soft_delete
 
-        get :index
+        get :index, params: { category_id: category1.id }
 
+        expect(assigns(:category)).to   eq(category1)
+        expect(assigns(:items)).to      include(item2)
         expect(assigns(:categories)).to include(category1, category2)
-        expect(assigns(:items)).to include(item2, item3, item4)
-      end
-    end
-
-    context "when a cateogry is deleted" do
-      it "does not include soft_deleted categories" do
-        signed_in_user :root
-        category1.soft_delete
-
-        get :index
-
-        expect(assigns(:categories)).not_to eq(category1)
       end
     end
   end
@@ -47,11 +38,10 @@ Rspec.describe ItemsController, type: :controller do
     it "returns the correct items" do
       signed_in_user :root
 
-      item2.soft_delete
-
       get :deleted
 
-      expect(assigns(:items)).to eq([item2])
+      expect(assigns(:items)).to eq([deleted_item])
+      expect(assigns(:categories)).to include(category1, category2)
     end
   end
 end
