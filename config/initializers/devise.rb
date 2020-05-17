7de870a9a56ff8a2bc07cdfa4a3c35a8f16bae3f
Tek Hudson <tek.hudson@gmail.com>
2020-05-16 22:45:56 -0700

    Update Devise secret key to use ENV var

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,9 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  # On Heroku Devise.secret_key was failing.
+  config.secret_key = ENV["STOCKAID_DEVISE_SECRET_KEY"]
+
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
