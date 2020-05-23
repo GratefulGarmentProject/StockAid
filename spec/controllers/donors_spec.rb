496880636758b7eb6f2aba010ef58557c4660a56
Tek Hudson <tek.hudson@gmail.com>
2020-05-23 09:42:34 -0700

    Extemd donor controller testing

--- spec/controllers/donors_spec.rb
+++ spec/controllers/donors_spec.rb
@@ -7,28 +7,65 @@ describe DonorsController, type: :controller do
   let(:donor4) { donors(:troi) }
 
   describe "GET#index" do
-    it "returns the correct variables" do
+    it "returns all existing donors" do
       signed_in_user :root
-
       get :index
 
-      expect(assigns(:donors)).to eq([donor1, donor2, donor3, donor4])
+      expect(assigns(:donors)).to include(donor1, donor2, donor3, donor4)
     end
 
-    context "when there are deleted items in the category" do
-      it "does not include deleted items" do
-        donor1.soft_delete
+    it "does not include deleted donors" do
+      donor1.soft_delete
+
+      signed_in_user :root
+      get :index
+
+      expect(assigns(:donors)).to include(donor2, donor3, donor4)
+    end
+  end
+
+  describe "PATCH#update" do
+    context "when user is a superuser" do
+      it "is able to update all fields" do
+        new_params = { id: donor1.id, donor: {
+          name: "foo", primary_number: "1", secondary_number: "2", email: "bar",
+          external_id: 9999, external_type: "Individual",
+          addresses_attributes: { "0" => { address: "" } }
+        } }
+
+        expect(donor1.name).to             eq("Starfleet Command")
+        expect(donor1.primary_number).to   eq("(510) 555-1111")
+        expect(donor1.secondary_number).to eq("(510) 555-1112")
+        expect(donor1.email).to            eq("info@starfleet.com")
+        expect(donor1.external_id).to      eq(1)
+        expect(donor1.external_type).to    eq("Organization")
 
         signed_in_user :root
-        get :index
+        put :update, new_params
 
-        expect(assigns(:donors)).to eq([donor2, donor3, donor4])
+        donor1.reload
+        expect(donor1.name).to             eq("foo")
+        expect(donor1.primary_number).to   eq("1")
+        expect(donor1.secondary_number).to eq("2")
+        expect(donor1.email).to            eq("bar")
+        expect(donor1.external_id).to      eq(9999)
+        expect(donor1.external_type).to    eq("Individual")
+      end
+    end
+
+    context "when user is _not_ a superuser" do
+      it "fails due to permissions" do
+        signed_in_user :acme_root
+
+        expect do
+          put :update, params: { id: donor1.id, donor: { name: "Foo" } }
+        end.to raise_error(PermissionError)
       end
     end
   end
 
-  describe "GET#deleted items" do
-    it "returns the correct items" do
+  describe "GET#deleted" do
+    it "returns the correct donors" do
       donor1.soft_delete
 
       signed_in_user :root
