53bb6505bdad1bf7190956c10f56bb14785c09a8
Mike Virata-Stone <stonemj@gmail.com>
2020-05-25 16:15:34 -0700

    Remove failed experiment for getting the commit hash in Heroku

--- app/controllers/info_controller.rb
+++
@@ -1,5 +0,0 @@
-class InfoController < ApplicationController
-  def index
-    render json: { version: Rails.application.config.version }
-  end
-end
--- config/initializers/info.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.version =
-  if Rails.env.development?
-    `git rev-parse HEAD`.strip
-  else
-    ENV.fetch("SOURCE_VERSION", "unknown")
-  end
