b3f217e1c3e4e7d1fc1d17f32c8de8e8dc17555e
Tek Hudson <tek.hudson@gmail.com>
2020-05-24 21:45:22 -0700

    Remove unneeded method

--- app/models/reports/value_by_donor.rb
+++ app/models/reports/value_by_donor.rb
@@ -34,7 +34,7 @@ module Reports
 
       def initialize(params, filter)
         @donor = Donor.find params[:donor]
-        @donations = filter.apply_date_filter(Donation.for_donor(donor), :donation_date)
+        @donations = filter.apply_date_filter(donor.donations, :donation_date)
                            .includes(donation_details: :item).to_a.map(&:donation_details).flatten.group_by(&:item)
       end
 
