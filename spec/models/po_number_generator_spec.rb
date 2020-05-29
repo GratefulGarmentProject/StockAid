e008f17d84fe1bbc6efb461e03ff20bee10c9833
Tamara Temple <tamouse@gmail.com>
2020-05-29 13:57:24 -0500

    Add spec to make sure PO numbers generate correctly
    The past PO numbers for a given vendor may not include any digit parts.
    
    - add a test with a set of PO numbers with no digit parts to ensure a good PO number still comes out.

--- spec/models/po_number_generator_spec.rb
+++ spec/models/po_number_generator_spec.rb
@@ -29,13 +29,42 @@ RSpec.describe PoNumberGenerator do
           Purchase.create(
             user: user,
             vendor: vendor,
-            po: PoNumberGenerator.new(vendor.id).format_string(vendor.name.parameterize, x),
+            po: PoNumberGenerator.new(vendor.id).format_string(vendor.name.parameterize, x + 1),
             purchase_date: 1.day.ago
           )
         end
       end
       it "returns the next PO number in sequence" do
-        expect(PoNumberGenerator.new(vendor.id).generate).to match(/\AGAREKS-\d+\z/)
+        new_po_number = PoNumberGenerator.new(vendor.id).generate
+        expect(new_po_number).to match(/\AGAREKS-\d+\z/)
+        digits = new_po_number.split("-").last
+        expect(digits.to_i).to eq(11)
+      end
+    end
+    context "when none of the previous purchases PO numbers has a digits part" do
+      let!(:user) do
+        User.create(
+          email: "test@fake.com",
+          name: "test user",
+          primary_number: "555-1212",
+          password: "P4ssword"
+        )
+      end
+      before do
+        10.times do |x|
+          Purchase.create(
+            user: user,
+            vendor: vendor,
+            po: "PONUM#{'*' * x}",
+            purchase_date: 1.day.ago
+          )
+        end
+      end
+      it "returns the next PO number with a digits part" do
+        new_po_number = PoNumberGenerator.new(vendor.id).generate
+        expect(new_po_number).to match(/\AGAREKS-\d+\z/)
+        digits = new_po_number.split("-").last
+        expect(digits.to_i).to eq(1), digits
       end
     end
   end
