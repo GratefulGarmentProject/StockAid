fdffbf35afb1f0d5862e4ed660c5bb00a5afea1c
Mike Virata-Stone <stonemj@gmail.com>
2020-06-08 00:55:22 -0700

    Get interacting with the API working

--- config/initializers/netsuite.rb
+++ config/initializers/netsuite.rb
@@ -1,45 +1,50 @@
+def common_netsuite_config(config)
+  config.reset!
+
+  config.account ENV["STOCKAID_NETSUITE_ACCOUNT_ID"]
+  config.api_version ENV.fetch("STOCKAID_NETSUITE_API_VERSION", "2018_2")
+  config.read_timeout ENV.fetch("STOCKAID_NETSUITE_READ_TIMEOUT", 60).to_i
+  config.wsdl_domain NetSuite::Utilities.data_center_url(ENV["STOCKAID_NETSUITE_ACCOUNT_ID"]).sub(%r{\A\w+://}, "")
+end
+
 if %w(
   STOCKAID_NETSUITE_ACCOUNT_ID
+  STOCKAID_NETSUITE_APPLICATION_ID
   STOCKAID_NETSUITE_CONSUMER_KEY
   STOCKAID_NETSUITE_CONSUMER_SECRET
   STOCKAID_NETSUITE_TOKEN_ID
   STOCKAID_NETSUITE_TOKEN_SECRET
 ).all? { |x| ENV[x].present? }
   NetSuite.configure do
-    reset!
-
-    account          ENV["STOCKAID_NETSUITE_ACCOUNT_ID"]
-    api_version      ENV.fetch("STOCKAID_NETSUITE_API_VERSION", "2018_2")
+    common_netsuite_config(self)
 
     consumer_key     ENV["STOCKAID_NETSUITE_CONSUMER_KEY"]
     consumer_secret  ENV["STOCKAID_NETSUITE_CONSUMER_SECRET"]
     token_id         ENV["STOCKAID_NETSUITE_TOKEN_ID"]
     token_secret     ENV["STOCKAID_NETSUITE_TOKEN_SECRET"]
-
-    read_timeout     ENV.fetch("STOCKAID_NETSUITE_READ_TIMEOUT", 60).to_i
-    wsdl_domain      NetSuite::Utilities.data_center_url(ENV["STOCKAID_NETSUITE_ACCOUNT_ID"]).sub(%r{\A\w+://}, "")
   end
 
   Rails.logger.info "Initialized NetSuite integration with token authentication"
   Rails.application.config.netsuite_initialized = true
 elsif %w(
   STOCKAID_NETSUITE_ACCOUNT_ID
+  STOCKAID_NETSUITE_APPLICATION_ID
   STOCKAID_NETSUITE_EMAIL
   STOCKAID_NETSUITE_PASSWORD
   STOCKAID_NETSUITE_ROLE
 ).all? { |x| ENV[x].present? }
   NetSuite.configure do
-    reset!
+    common_netsuite_config(self)
 
-    account          ENV["STOCKAID_NETSUITE_ACCOUNT_ID"]
-    api_version      ENV.fetch("STOCKAID_NETSUITE_API_VERSION", "2018_2")
+    email     ENV["STOCKAID_NETSUITE_EMAIL"]
+    password  ENV["STOCKAID_NETSUITE_PASSWORD"]
+    role      ENV["STOCKAID_NETSUITE_ROLE"].to_i
 
-    email ENV["STOCKAID_NETSUITE_EMAIL"]
-    password ENV["STOCKAID_NETSUITE_PASSWORD"]
-    role ENV["STOCKAID_NETSUITE_ROLE"].to_i
-
-    read_timeout     ENV.fetch("STOCKAID_NETSUITE_READ_TIMEOUT", 60).to_i
-    wsdl_domain      NetSuite::Utilities.data_center_url(ENV["STOCKAID_NETSUITE_ACCOUNT_ID"]).sub(%r{\A\w+://}, "")
+    self.soap_header = {
+      "platformMsgs:ApplicationInfo" => {
+        "platformMsgs:applicationId" => ENV["STOCKAID_NETSUITE_APPLICATION_ID"]
+      }
+    }
   end
 
   Rails.logger.info "Initialized NetSuite integration with password authentication"
