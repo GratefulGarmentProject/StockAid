412d14a3c7cf97b7ede519e073308ac4bf6401bc
Tek Hudson <tek.hudson@gmail.com>
2020-06-25 19:28:00 -0700

    Update Rack timeout setup

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,3 +1,6 @@
-Rack::Timeout, service_timeout: (ENV["STOCKAID_RACK_TIMEOUT_SECONDS"].presence || 20).to_i
+Rails.application.config.middleware.insert_before(
+  Rack::Runtime, Rack::Timeout,
+  service_timeout: (ENV["STOCKAID_RACK_TIMEOUT_SECONDS"].presence || 20).to_i
+)
 Rack::Timeout::Logger.logger = Logger.new(Rails.env.production? ? STDOUT : "log/timeout.log")
 Rack::Timeout::Logger.logger.level = Logger::ERROR
