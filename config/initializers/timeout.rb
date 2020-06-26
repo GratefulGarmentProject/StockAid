98c2b394034ceebef29233789f456fc3a3c2ca0f
Tek Hudson <tek.hudson@gmail.com>
2020-06-25 18:18:55 -0700

    Update Rack timeout setup

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,4 +1,3 @@
-# Recommended in https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#timeout
-Rack::Timeout.timeout = (ENV["STOCKAID_RACK_TIMEOUT_SECONDS"].presence || 20).to_i # seconds
+Rack::Timeout, service_timeout: (ENV["STOCKAID_RACK_TIMEOUT_SECONDS"].presence || 20).to_i
 Rack::Timeout::Logger.logger = Logger.new(Rails.env.production? ? STDOUT : "log/timeout.log")
 Rack::Timeout::Logger.logger.level = Logger::ERROR
