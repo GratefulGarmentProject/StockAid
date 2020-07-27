fc650dcece3d1c7545f1b5734b9107c725863085
Tek Hudson <tek.hudson@gmail.com>
2020-07-27 08:31:14 -0700

    Add notes column to purchases table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200615021643) do
+ActiveRecord::Schema.define(version: 20200727152938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -243,6 +243,7 @@ ActiveRecord::Schema.define(version: 20200615021643) do
     t.decimal "tax", precision: 8, scale: 2, default: "0.0"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "notes"
     t.index ["user_id"], name: "index_purchases_on_user_id"
     t.index ["vendor_id"], name: "index_purchases_on_vendor_id"
   end
