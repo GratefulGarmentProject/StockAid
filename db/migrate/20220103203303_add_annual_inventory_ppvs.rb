355653e6ab8b51a54ec1573ad00c48e40de03f7f
Tek Hudson <tek.hudson@gmail.com>
2022-01-18 19:46:12 -0800

    Add missing column to table

--- db/migrate/20220103203303_add_annual_inventory_ppvs.rb
+++ db/migrate/20220103203303_add_annual_inventory_ppvs.rb
@@ -3,7 +3,8 @@ class AddAnnualInventoryPpvs < ActiveRecord::Migration[5.1]
     create_table :annual_inventory_ppvs do |t|
       t.integer :year
       t.decimal :total_inventory_value, precision: 13, scale: 2
-      t.decimal :inventory_ppv, precision: 13, scale: 2
+      t.decimal :annual_ppv, precision: 13, scale: 2
+      t.decimal :real_inventory_value, precision: 13, scale: 2
 
       t.timestamps null: false
     end
