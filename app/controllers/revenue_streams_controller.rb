efc747254cc6512004290dd2681b60fe4396f764
Mike Virata-Stone <stonemj@gmail.com>
2022-04-26 00:05:14 -0700

    A bit of refactoring and add external id to revenue streams

--- app/controllers/revenue_streams_controller.rb
+++ app/controllers/revenue_streams_controller.rb
@@ -8,40 +8,49 @@ class RevenueStreamsController < ApplicationController
   end
 
   def show
-    return if revenue_stream.present?
+    @revenue_stream = RevenueStream.active.find_by(id: params[:id])
+    return if @revenue_stream.present?
 
     flash[:error] = "No active Revenue Stream with that id"
     redirect_to revenue_streams_path
   end
 
   def update
-    if revenue_stream.update!(revenue_stream_params)
+    @revenue_stream = RevenueStream.active.find_by(id: params[:id])
+
+    if @revenue_stream.update!(revenue_stream_params)
       flash[:success] = "Revenue Stream updated"
       redirect_to revenue_streams_path
     else
-      flash[:error] = revenue_stream.errors.full_messages.join(". ")
-      redirect_to revenue_stream_path(revenue_stream)
+      flash[:error] = @revenue_stream.errors.full_messages.join(". ")
+      redirect_to revenue_stream_path(@revenue_stream)
     end
   end
 
-  def create
-    revenue_stream = RevenueStream.create(name: "New Revenue Stream")
+  def new
+    @revenue_stream = RevenueStream.new(name: "New Revenue Stream")
+  end
 
-    if revenue_stream.errors.any?
-      flash[:error] = revenue_stream.errors.full_messages.join(". ")
-      redirect_to revenue_streams_path
+  def create
+    @revenue_stream = RevenueStream.create(revenue_stream_params)
+
+    if @revenue_stream.errors.any?
+      flash.now[:error] = @revenue_stream.errors.full_messages.join(". ")
+      render :new
     else
-      redirect_to revenue_stream_path(revenue_stream)
+      redirect_to revenue_streams_path
     end
   end
 
   def destroy
-    if revenue_stream.soft_delete
+    @revenue_stream = RevenueStream.active.find_by(id: params[:id])
+
+    if @revenue_stream.soft_delete
       flash[:success] = "Revenue Stream soft deleted"
       redirect_to revenue_streams_path
     else
-      flash[:error] = revenue_stream.errors.full_messages.join(". ")
-      redirect_to revenue_stream_path(revenue_stream)
+      flash[:error] = @revenue_stream.errors.full_messages.join(". ")
+      redirect_to revenue_stream_path(@revenue_stream)
     end
   end
 
@@ -56,11 +65,7 @@ class RevenueStreamsController < ApplicationController
 
   private
 
-  def revenue_stream
-    @revenue_stream ||= RevenueStream.active.find_by(id: params[:id])
-  end
-
   def revenue_stream_params
-    params.require(:revenue_stream).permit(:name)
+    params.require(:revenue_stream).permit(:name, :external_id)
   end
 end
