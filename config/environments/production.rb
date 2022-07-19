3f58174b2fe1ee21f193877d50ca71c4e8ad7417
Mike Virata-Stone <stonemj@gmail.com>
2022-07-18 23:02:06 -0700

    Disable ActiveStorage, since we don't use it at the moment and it probably won't work with local storage on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # StockAid NOTE: ActiveStorage is currently disabled
+  # config.active_storage.service = :local
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
