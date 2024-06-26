9754fd9e5bf5b7b42071a31091d3ce50308733bc
Mike Virata-Stone <stonemj@gmail.com>
2024-06-25 22:15:41 -0700

    Update to shakapacker from webpack

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem "rack-timeout"
 gem "select2-rails"
 gem "spreadsheet", "~> 1.1", ">= 1.1.2"
 gem "stateful_enum"
-gem "webpacker", "6.0.0.rc.6"
+gem "shakapacker", "6.0.0.rc.13"
 
 group :development, :test do
   gem "capybara"
--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,7 +1,8 @@
-const { webpackConfig, merge } = require('@rails/webpacker')
+const { webpackConfig, merge } = require('shakapacker')
 const coffee = require('./loaders/coffee')
 const webpack = require('webpack')
 
+// See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
 const customConfig = {
   plugins: [
     new webpack.ProvidePlugin({
