9a55a0014b1dcb374da03de726da2729286f5a4d
Mike Virata-Stone <stonemj@gmail.com>
2025-01-27 21:58:09 -0800

    Fix importing organization from NetSuite by allowing adding programs, and get the county when importing, but if it is not found, only create the county if it looks like a California county, require manual setting otherwise

--- app/models/net_suite_integration/organization_importer.rb
+++ app/models/net_suite_integration/organization_importer.rb
@@ -1,9 +1,11 @@
 module NetSuiteIntegration
   class OrganizationImporter
-    attr_reader :netsuite_id, :netsuite_org
+    attr_reader :netsuite_id, :netsuite_org, :program_ids
 
     def initialize(params)
       @netsuite_id = params.require(:external_id).to_i
+      org_params = params.require(:organization).permit(program_ids: [])
+      @program_ids = org_params[:program_ids]
     end
 
     def import
@@ -31,20 +33,38 @@ module NetSuiteIntegration
       raise ActiveRecord::RecordInvalid, record_for_error
     end
 
-    def create_organization
-      Organization.create! do |organization|
-        organization.name = netsuite_org.name
-        organization.external_id = netsuite_org.netsuite_id
-        organization.external_type = netsuite_org.type
-        organization.email = netsuite_org.email
-        organization.phone_number = netsuite_org.phone
+    def create_organization # rubocop:disable Metrics/AbcSize
+      Organization.transaction do
+        Organization.create! do |organization|
+          organization.name = netsuite_org.name
+          organization.external_id = netsuite_org.netsuite_id
+          organization.external_type = netsuite_org.type
+          organization.email = netsuite_org.email
+          organization.phone_number = netsuite_org.phone
+          organization.program_ids = program_ids
 
-        netsuite_address = netsuite_org.address
-        if netsuite_address
-          # netsuite_address will be a hash with the proper address parts
-          organization.addresses.build(netsuite_address)
+          netsuite_address = netsuite_org.address
+          if netsuite_address
+            # netsuite_address will be a hash with the proper address parts
+            organization.addresses.build(netsuite_address)
+          end
+
+          county = fetch_county
+          organization.organization_county = county if county
         end
       end
+    end
+
+    def fetch_county
+      return nil if netsuite_org.county_id.blank?
+
+      county = County.where(external_id: netsuite_org.county_id).first
+      return county if county
+
+      match = /\ACalifornia : (.*) County\z/.match(netsuite_org.county_name)
+
+      return unless match
+      County.create!(name: match[1], external_id: netsuite_org.county_id)
     end
   end
 end
